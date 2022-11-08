d5920aed933ed4a90765329de33363967e2cdff6
Rayanworkout <rayan13170@protonmail.com>
2022-11-08 01:36:28 +0100

    up

--- bot.py
+++ bot.py
@@ -7,12 +7,18 @@ from discord.ext import commands
 from discord.ext.commands.errors import (CheckFailure, CommandNotFound,
                                          MissingRequiredArgument)
 
+from tracker import number_following_stored, get_time
+
 bot = commands.Bot(command_prefix="!", help_command=None, case_insensitive=True, intents=discord.Intents.all())
 
 auth = tweepy.OAuthHandler('qi0FcmJWMv1T6x8WYhvD6M8d3', 'HI9gHHJNiTXwBWuJlsXzFUDjOhe2t7gBHXbptCNE1L9JvzhFU6')
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
 client = tweepy.Client(bearer_token="AAAAAAAAAAAAAAAAAAAAADhdUQEAAAAAaMGUYLBUc9APzkNTXIGNCPpDyF4%3DSFWrzBewEaWgRqDssuvfsQsKjpPMx1zR55QJSWratpDSejEb2T")
+
+
+# TODO AFFICHER CHAQUE TAG ET LES COMPTES TRACKÉS
+
 
 #############################################################################################################
 
@@ -32,6 +38,7 @@ async def on_command_error(ctx, error):
             description="You need to specify something after the command !  :x:",
             color=discord.Colour.blue(),
         )
+        print(f'[{get_time()}] Error: empty command')
         await ctx.reply(embed=embed)
     elif isinstance(error, (CheckFailure, CommandNotFound)):
         pass
@@ -47,20 +54,39 @@ def checkAuthorized(ctx):
     admin commands"""
     return ctx.message.author.id in [
         828233789594927115,
-        965594724276793474
+        965594724276793474,
+        147053194801971200,
+        1007398137062760549
     ]
 
 #############################################################################################################
 
+
 @bot.command()
 @commands.check(checkAuthorized)
-async def track(ctx, *args):
+async def spyTags(ctx):
+    with open('db.json', 'r') as f:
+        db = json.load(f)
+
+    tags = '\n- '.join(sorted(set(db[user]['Tag'].upper() for user in db)))
+    embed = discord.Embed(
+        description=f"**Current Tags are:\n\n- {tags}**",
+        color=discord.Colour.blue(),
+        )
+    
+    return await ctx.reply(embed=embed)
+
+
+@bot.command()
+@commands.check(checkAuthorized)
+async def spy(ctx, *args):
     
     if len(args) != 2:
         embed = discord.Embed(
         description="**You need to specify a Twitter username and a Tag with this command  :x:**",
         color=discord.Colour.blue(),
         )
+        print(f'[{get_time()}] Error: track: missing tag')
         return await ctx.reply(embed=embed)
     
     username = args[0]
@@ -85,6 +111,7 @@ async def track(ctx, *args):
         description=f"**{username} is not a valid Twitter username  :x:**",
         color=discord.Colour.blue(),
         )
+        print(f'[{get_time()}] Error: track: @{username} is not a valid Twitter username')
         return await ctx.reply(embed=embed)
     
     except Exception as err:
@@ -96,16 +123,18 @@ async def track(ctx, *args):
         description=f"**@{username} is already tracked !**",
         color=discord.Colour.blue(),
         )
+        print(f'[{get_time()}] Error: track: @{username} is already tracked')
         return await ctx.reply(embed=embed)
     
     try:
-        follows = client.get_users_following(id=user_id, max_results=50)
+        follows = client.get_users_following(id=user_id, max_results=number_following_stored)
         follows_id = [user.id for user in follows.data]
     except TypeError:
         embed = discord.Embed(
         description=f"**Could not get followers of @{username}, this account may be private ...**",
         color=discord.Colour.blue(),
         )
+        print(f'[{get_time()}] Error: track: could not get @{username} followers')
         return await ctx.reply(embed=embed)
     
     db[user_id] = {
@@ -135,13 +164,14 @@ async def track(ctx, *args):
     embed.add_field(name="Following", value=friends_count, inline=False)
     embed.add_field(name="Bio", value=bio if bio else "/", inline=False)
     
+    print(f'[{get_time()}] track: @{username} is now tracked')
     return await ctx.reply(embed=embed)
     
 
 
 @bot.command()
 @commands.check(checkAuthorized)
-async def untrack(ctx, arg):
+async def unSpy(ctx, arg):
     
     username = arg
     
@@ -155,6 +185,7 @@ async def untrack(ctx, arg):
         description=f"**@{username} is not tracked !**",
         color=discord.Colour.blue(),
         )
+        print(f'[{get_time()}] Error: untrack: @{username} is not tracked')
         return await ctx.reply(embed=embed)
     
     elif username in tracked_usernames:
@@ -171,6 +202,7 @@ async def untrack(ctx, arg):
         description=f"**@{username} is no longer tracked :white_check_mark:**",
         color=discord.Colour.blue(),
         )
+        print(f'[{get_time()}] untrack: @{username} is no longer tracked')
         return await ctx.reply(embed=embed)
 
 
@@ -180,4 +212,3 @@ if not os.path.isfile('db.json'):
         json.dump({}, f, indent=4)
 
 bot.run("MTAzODEyNDUxODI2ODQ4NTcyMw.G30rEI.ZAIi-oi0s9ztR6QUEn8honbeNSQXwTi6xvDwHA")
-
--- tracker.py
+++ tracker.py
@@ -1,10 +1,10 @@
 import json
-import os
 import time
 
 import requests
 import tweepy
 from discord_webhook import DiscordEmbed, DiscordWebhook
+from datetime import datetime
 
 ###################################################################################################
 
@@ -19,89 +19,104 @@ client = tweepy.Client(bearer_token="AAAAAAAAAAAAAAAAAAAAADhdUQEAAAAAaMGUYLBUc9A
 auth = tweepy.OAuthHandler('qi0FcmJWMv1T6x8WYhvD6M8d3', 'HI9gHHJNiTXwBWuJlsXzFUDjOhe2t7gBHXbptCNE1L9JvzhFU6')
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
+number_following_per_request = 50
+number_following_stored = 100
+
 #######################################################################################################
+
+def get_time():
+    return datetime.now().strftime('%D %H:%M:%S')
 
 def compare():  
     while True:
-        with open('db.json', 'r') as f:
-            db = json.load(f)
+        try:
+            with open('db.json', 'r') as f:
+                db = json.load(f)
 
-        for tracked_account in db.keys():
-            lastFollowings = client.get_users_following(id=tracked_account, max_results=50)
-            newly_following_users = lastFollowings.data
+            for tracked_account in db.keys():
+                lastFollowings = client.get_users_following(id=tracked_account, max_results=number_following_per_request)
+                newly_following_users = lastFollowings.data
+                
+                old_follow_list = db[tracked_account]["follows"]
+
+                differences = [value for value in newly_following_users if value.id not in old_follow_list]
             
-            
-            old_follow_list = db[tracked_account]["follows"]
+                if differences:
+                    del db[tracked_account]["follows"][len(old_follow_list) - len(differences):]
+                    db[tracked_account]["follows"] = [value.id for value in differences] + db[tracked_account]["follows"]
 
-            differences = [value for value in newly_following_users if value.id not in old_follow_list]
-        
-            if differences:
-                del db[tracked_account]["follows"][len(old_follow_list) - len(differences):]
-                db[tracked_account]["follows"] = [value.id for value in differences] + db[tracked_account]["follows"]
+                    webhook.remove_embeds()
 
-                for user in differences:
-                    
-                    twitter_user = api.get_user(user_id=user.id)
-                    
-                    username = twitter_user.screen_name
-                    followers_count = twitter_user.followers_count
-                    friends_count = twitter_user.friends_count
-                    bio = twitter_user.description
-                    profile_picture = twitter_user.profile_image_url_https
-                    created_at = twitter_user.created_at
-                    
-                    if twitter_user.url:
-                        profile_bio_url = requests.get(f"https://unshort.herokuapp.com/api/?url={twitter_user.url}").json()["longUrl"]
-                    else:
-                        profile_bio_url = "None"
-                    
-                    tag = db[tracked_account]['Tag'].upper()
-                    
-                    if tag == "ALPHA":
-                        color = "e81224"
-                    elif tag == "CONTRIBUTOR":
-                        color = "f7630c"
-                    elif tag == "OG":
-                        color = "fff100"
-                    elif tag == "SHITCOIN":
-                        color = "16c60c"
-                    elif tag == "DEFI":
-                        color = "0078d7"
-                    elif tag == "NFT":
-                        color = "886ce4"
-                    elif tag == "VC":
-                        color = "ffffff"
-                    
-                    description = (f"\n:blue_circle:  **Followers:** {followers_count}\n\n:orange_circle:  **Following:**" 
-                                   f" {friends_count}\n\n**Bio:** {bio}\n\n:link:  **Link in Bio**\n{profile_bio_url}\n\n"
-                                   f":hourglass_flowing_sand: **Profile Creation Date**\n{created_at.strftime('%d/%m/%Y')}")
-                    
-                    embed = DiscordEmbed(title=username, description=description,
-                                         url=f'https://twitter.com/{username}', color=color)
-                    
-                    embed.set_author(name=f"New Follow for {db[tracked_account]['username']}  [{db[tracked_account]['Tag'].upper()}]",
-                                     icon_url=db[tracked_account]['profile_picture'])    
-                    
-                    embed.set_thumbnail(url=profile_picture)
+                    for user in differences:
                         
-                    embed.set_timestamp()
-                    
-                    webhook.add_embed(embed)
-                    webhook.execute()
+                        twitter_user = api.get_user(user_id=user.id)
+                        
+                        username = twitter_user.screen_name
+                        followers_count = twitter_user.followers_count
+                        friends_count = twitter_user.friends_count
+                        bio = twitter_user.description
+                        profile_picture = twitter_user.profile_image_url_https
+                        created_at = twitter_user.created_at
+                        
+                        if twitter_user.url:
+                            profile_bio_url = requests.get(f"https://unshort.herokuapp.com/api/?url={twitter_user.url}").json()["longUrl"]
+                        else:
+                            profile_bio_url = "None"
+                        
+                        tag = db[tracked_account]['Tag'].upper()
+                        
+                        if tag == "ALPHA":
+                            color = "e81224"
+                        elif tag == "CONTRIBUTOR":
+                            color = "f7630c"
+                        elif tag == "OG":
+                            color = "fff100"
+                        elif tag == "SHITCOIN":
+                            color = "16c60c"
+                        elif tag == "DEFI":
+                            color = "0078d7"
+                        elif tag == "NFT":
+                            color = "886ce4"
+                        elif tag == "VC":
+                            color = "ffffff"
+                        else:
+                            color = '000000'
+                        
+                        description = (f"\n:blue_circle:  **Followers:** {followers_count}\n\n:orange_circle:  **Following:**" 
+                                    f" {friends_count}\n\n**Bio:** {bio}\n\n:link:  **Link in Bio**\n{profile_bio_url}\n\n"
+                                    f":hourglass_flowing_sand: **Profile Creation Date**\n{created_at.strftime('%d/%m/%Y')}")
+                        
+                        webhook.remove_embeds()
 
-                    os.remove('db.json')
-                    with open('db.json', 'w+') as f:
+                        embed = DiscordEmbed(title=username, description=description,
+                                            url=f'https://twitter.com/{username}', color=color)
+                        
+                        embed.set_author(name=f"New Follow for {db[tracked_account]['username']}  [{db[tracked_account]['Tag'].upper()}]",
+                                        icon_url=db[tracked_account]['profile_picture'])    
+                        
+                        embed.set_thumbnail(url=profile_picture)
+                            
+                        embed.set_timestamp()
+                        
+                        webhook.add_embed(embed)
+
+                        webhook.execute()
+
+                    with open('db.json', 'w') as f:
                         json.dump(db, f, indent=4)
-                    
-                    print(f"New follow(s) for {db[tracked_account]['username']}: {' '.join([user.username for user in differences])}")
                         
-            else:
-                print(f"No new follow for {db[tracked_account]['username']}")
+                    print(f"[{get_time()}] New follow(s) for {db[tracked_account]['username']}: {' '.join([user.username for user in differences])}")
+                            
+                else:
+                    print(f"[{get_time()}] No new follow for {db[tracked_account]['username']}")
+                
+                time.sleep(200)
+        except Exception as e:
+            requests.get("https://api.telegram.org/bot1768068100:AAHVGEdeItHypLHBfqmMoqdqhX4KdgO08Gc/"
+                 "sendMessage?chat_id=901170303&text={}".format("Error with Twitter Tracker: " + str(e)))
             
-            time.sleep(120)
-      
+            time.sleep(900)
+
 
 if '__main__' == __name__:
     compare()
-
-
